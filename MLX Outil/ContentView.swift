55c5bc7fb59985fe5cd7525f0edea267033191d0
Rudrank Riyam <rudrankriyam@gmail.com>
2024-12-01 02:28:39 +0530

    Initial project setup for MLX Outil

---
+++ MLX Outil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MLX Outil
+//
+//  Created by Rudrank Riyam on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ MLX Outil/MLXOutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLXOutilApp.swift
+//  MLX Outil
+//
+//  Created by Rudrank Riyam on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MLXOutilApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
