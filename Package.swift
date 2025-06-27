eb2a928b8374b17e0a7650ee11352cf77d7bf42a
Rudrank Riyam <rudrankriyam@gmail.com>
2025-06-27 14:23:03 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "MLXTools",
-  platforms: [.macOS(.v14), .iOS(.v17)],
+  platforms: [.macOS(.v14), .iOS(.v17), .visionOS(.v1)],
   products: [
     .library(
       name: "MLXTools",
